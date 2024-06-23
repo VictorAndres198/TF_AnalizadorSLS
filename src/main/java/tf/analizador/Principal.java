e27bee172d6e115acab398e54f7052f161de89f5
VictorAndres198 <100662467+VictorAndres198@users.noreply.github.com>
2024-06-22 19:26:44 -0500

    Haciendo cambios en "Principal.java"
    Los cambios hacen que no sea necesario modificar las direcciones del Lexer.flex, LexerCup.flex

--- src/main/java/tf/analizador/Principal.java
+++ src/main/java/tf/analizador/Principal.java
@@ -1,59 +1,65 @@
 package tf.analizador;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import jflex.Main;
-import java_cup.*;
-import jflex.*;
-
 public class Principal {
 
     public static void main(String[] args) {
-    	String pathLexer = "C:/Users/Alejo/eclipse-workspace/TF_AnalizadorSLS/src/main/java/tf/analizador/Lexer.flex";
-    	String pathLexerCup = "C:/Users/Alejo/eclipse-workspace/TF_AnalizadorSLS/src/main/java/tf/analizador/LexerCup.flex";
-    	String pathSyntaxCup = "C:/Users/Alejo/eclipse-workspace/TF_AnalizadorSLS/src/main/java/tf/analizador/Sintax.cup";
+        // Obtener la ruta del proyecto
+        String projectPath = System.getProperty("user.dir").replace("\\", "/");
+
+        // Construir las rutas basadas en la ruta del proyecto
+        String pathLexer = projectPath + "/src/main/java/tf/analizador/Lexer.flex";
+        String pathLexerCup = projectPath + "/src/main/java/tf/analizador/LexerCup.flex";
+        String pathSyntaxCup = projectPath + "/src/main/java/tf/analizador/Sintax.cup";
         String[] rutaS = {"-parser", "Sintax", pathSyntaxCup};
+
         try {
-			generar(pathLexer, pathLexerCup, rutaS);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            generar(pathLexer, pathLexerCup, rutaS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
-	public static void generar(String pathLexer, String pathLexerCup, String[] rutaS) throws IOException, Exception {
+
+    public static void generar(String pathLexer, String pathLexerCup, String[] rutaS) throws IOException, Exception {
         File archivo;
+
+        // Generar Lexer.flex
         archivo = new File(pathLexer);
         jflex.Main.generate(archivo);
+
+        // Generar LexerCup.flex
         archivo = new File(pathLexerCup);
         jflex.Main.generate(archivo);
+
+        // Ejecutar JavaCUP
         java_cup.Main.main(rutaS);
-        
-        String pathSym = "C:/Users/Alejo/eclipse-workspace/TF_AnalizadorSLS/src/main/java/tf/analizador/sym.java";
+
+        // Mover sym.java
+        String projectPath = System.getProperty("user.dir").replace("\\", "/");
+        String pathSym = projectPath + "/src/main/java/tf/analizador/sym.java";
         Path rutaSym = Paths.get(pathSym);
         if (Files.exists(rutaSym)) {
             Files.delete(rutaSym);
         }
-        
         Files.move(
-                Paths.get("C:/Users/Alejo/eclipse-workspace/TF_AnalizadorSLS/sym.java"), 
+                Paths.get(projectPath + "/sym.java"),
                 Paths.get(pathSym)
         );
-        
-        
-        String pathSintax = "C:/Users/Alejo/eclipse-workspace/TF_AnalizadorSLS/src/main/java/tf/analizador/Sintax.java";
+
+        // Mover Sintax.java
+        String pathSintax = projectPath + "/src/main/java/tf/analizador/Sintax.java";
         Path rutaSin = Paths.get(pathSintax);
         if (Files.exists(rutaSin)) {
             Files.delete(rutaSin);
         }
         Files.move(
-                Paths.get("C:/Users/Alejo/eclipse-workspace/TF_AnalizadorSLS/Sintax.java"), 
+                Paths.get(projectPath + "/Sintax.java"),
                 Paths.get(pathSintax)
         );
     }
-    
-    
-
 }
